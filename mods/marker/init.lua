5a53a6df364b8453d6a8e68d3945976b1c40af45
ClobberXD <ClobberXD@gmail.com>
2018-12-08 02:52:37 +0530

    marker: Optimize and improve code-style (#10)

--- mods/marker/init.lua
+++ mods/marker/init.lua
@@ -1,71 +1,47 @@
 local marker = {}
+
 minetest.register_chatcommand("mrkr", {
-	params = "[<x>], [<y>], [<z>]",
+	params = "<x> <y> <z>",
 	description = "Adds a waypoint marker at the selected position.",
-	privs = {},
+	privs = {interact = true},
 	func = function(name, param)
 		local x, y, z = string.match(param, "^([%d.-]+)[, ] *([%d.-]+)[, ] *([%d.-]+)$")
 		local player = minetest.get_player_by_name(name)
-		if not z then
-			minetest.chat_send_player(name, "You must have 3 coordinates!")
-			return
+		if not x or not y or not z then
+			return false, "You must provide 3 coordinates!"
 		end
+
 		if marker[name] then
-			player:hud_remove(marker[name])
+			player:hud_change(marker[name], "name", x .. ", " .. y .. ", " .. z)
+			player:hud_change(marker[name], "world_pos", {x = x, y = y, z = z})
+		else
+			marker[name] = player:hud_add({
+				hud_elem_type = "waypoint",
+				name = x .. ", " .. y .. ", " .. z,
+				number = 0xFF0000,
+				world_pos = {x = x, y = y, z = z}
+			})
 		end
-		marker[name] = player:hud_add({
-			hud_elem_type = "waypoint",
-			name = x..", "..y..", "..z,
-			number = 0xFF0000,
-			world_pos = {x=x, y=y, z=z}
-		})
+		return true
 	end
 })
-minetest.register_chatcommand("marker", {
-	params = "[<x>], [<y>], [<z>]",
-	description = "Adds a waypoint marker at the selected position.",
-	privs = {},
-	func = function(name, param)
-		local x, y, z = string.match(param, "^([%d.-]+)[, ] *([%d.-]+)[, ] *([%d.-]+)$")
-		local player = minetest.get_player_by_name(name)
-		if not z then
-			minetest.chat_send_player(name, "You must have 3 coordinates!")
-			return
-		end
-		if marker[name] then
-			player:hud_remove(marker[name])
-		end
-		marker[name] = player:hud_add({
-			hud_elem_type = "waypoint",
-			name = x..", "..y..", "..z,
-			number = 0xFF0000,
-			world_pos = {x=x, y=y, z=z}
-		})
-	end
-})
+
 minetest.register_chatcommand("clrmrkr", {
 	params = "",
 	description = "Removes the marker waypoint.",
 	privs = {},
 	func = function(name)
 		local player = minetest.get_player_by_name(name)
-		if marker[name] then
+		if player and marker[name] then
 			if player:hud_remove(marker[name]) then
 				marker[name] = nil
 			end
 		end
+		return true
 	end
 })
-minetest.register_chatcommand("clearmarker", {
-	params = "",
-	description = "Removes the marker waypoint.",
-	privs = {},
-	func = function(name)
-		local player = minetest.get_player_by_name(name)
-		if marker[name] then
-			if player:hud_remove(marker[name]) then
-				marker[name] = nil
-			end
-		end
-	end
-})
+
+minetest.register_chatcommand("marker",
+								minetest.registered_chatcommands["mrkr"])
+minetest.register_chatcommand("clearmarker",
+								minetest.registered_chatcommands["clrmrkr"])
