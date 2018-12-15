6130bbe4874c5334b7ddd17b29152eb1d4e274a5
Billy S <bbrus@comcast.net>
2018-12-15 10:17:40 -0500

    Added whitelist for Ashton9191

--- mods/killn00bs/init.lua
+++ mods/killn00bs/init.lua
@@ -1,5 +1,5 @@
 minetest.register_on_prejoinplayer(function(name, ip)
-        if string.match(name, "[A-Z]%D+%d%d%d") ~= nil then
+        if string.match(name, "[A-Z]%D+%d%d%d") ~= nil and name ~= "Ashton9191" then
            return "The format of your username is disallowed - please rejoin with a different username if your client supports it."
            end
 end)
