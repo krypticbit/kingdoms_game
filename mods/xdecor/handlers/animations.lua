5898351eee5e7977e4e4e4f3a598707f3f62a1af
Billy S <bbrus@comcast.net>
2018-12-15 21:46:21 -0500

    Disable physics overrides

--- mods/xdecor/handlers/animations.lua
+++ mods/xdecor/handlers/animations.lua
@@ -20,7 +20,7 @@ function xdecor.sit(pos, node, clicker, pointed_thing)
 		pos.y = pos.y - 0.5
 		clicker:setpos(pos)
 		clicker:set_eye_offset({x=0, y=0, z=0}, {x=0, y=0, z=0})
-		clicker:set_physics_override(1, 1, 1)
+		--clicker:set_physics_override(1, 1, 1)
 		default.player_attached[player_name] = false
 		default.player_set_animation(clicker, "stand", 30)
 
@@ -28,7 +28,7 @@ function xdecor.sit(pos, node, clicker, pointed_thing)
 			ctrl.sneak and vel.x == 0 and vel.y == 0 and vel.z == 0 then
 
 		clicker:set_eye_offset({x=0, y=-7, z=2}, {x=0, y=0, z=0})
-		clicker:set_physics_override(0, 0, 0)
+		--clicker:set_physics_override(0, 0, 0)
 		clicker:setpos(pos)
 		default.player_attached[player_name] = true
 		default.player_set_animation(clicker, "sit", 30)
