17bf01c74b6cac94a2546ad8c4c7ead6286a8be3
Billy S <bbrus@comcast.net>
2019-01-02 15:08:16 -0500

    Added formspec for KO'd players

--- mods/knockout/init.lua
+++ mods/knockout/init.lua
@@ -10,11 +10,67 @@ knockout.tools = {}
 -- Create mod storage
 knockout.storage = minetest.get_mod_storage()
 
--- Created locals
+-- Create locals
 local knockout_huds = {}
+local player_invs_detached = {}
+
+local can_edit_player_inv = function(name, count)
+   if minetest.get_player_by_name(name) and knockout.knocked_out[name] then -- player is online and knocked out
+      return count
+   end
+   return 0
+end
+
+local update_player_inv = function(dInv, victimName)
+   local v = minetest.get_player_by_name(victimName)
+   if v == nil then return end
+   local vInv = minetest.get_inventory({type='player', name = victimName})
+   vInv:set_list("main", dInv:get_list("main"))
+   local aInv = minetest.get_inventory({type=  "detached", name = victimName .. "_armor"})
+   aInv:set_list("armor", dInv:get_list("armor"))
+   armor:set_player_armor(v)
+end
+
+local get_knocked_out_fs = function(victimName)
+   if player_invs_detached[victimName] == nil then
+      local pInv = minetest.get_inventory({type='player', name = victimName})
+      local d = minetest.create_detached_inventory("ko_" .. victimName, {
+         allow_move = function(inv, from_list, from_index, to_list, to_index, count, player)
+            -- Prevent moving inventory items into the armor slots
+            if from_list ~= to_list then return 0 end
+            return can_edit_player_inv(victimName, count)
+         end,
+         allow_put = function(inv, listname, index, stack, player)
+            return can_edit_player_inv(victimName, stack:get_count())
+         end,
+         allow_take = function(inv, listname, index, stack, player)
+            return can_edit_player_inv(victimName, stack:get_count())
+         end,
+         on_move = function(inv, from_list, from_index, to_list, to_index, count, player)
+            update_player_inv(inv, victimName)
+         end,
+         on_put = function(inv, listname, index, stack, player)
+            update_player_inv(inv, victimName)
+         end,
+         on_take = function(inv, listname, index, stack, player)
+            update_player_inv(inv, victimName)
+         end
+      })
+      local aInv = minetest.get_inventory({type = "detached", name = victimName .. "_armor"})
+      d:set_list("main", pInv:get_list("main"))
+      d:set_list("armor", aInv:get_list("armor"))
+      player_invs_detached[victimName] = d
+   else
+      local d = player_invs_detached[victimName]
+   end
+   local fs = "size[11,9]" ..
+   "list[detached:ko_" .. victimName .. ";main;0,0;8,4;]" ..
+   "list[detached:ko_" .. victimName .. ";armor;9,0;2,3;]" ..
+   "list[current_player;main;0,5;8,4;]"
+   return fs
+end
 
 -- Register entity
-   
 minetest.register_entity("knockout:entity", {
 	hp_max = 1000,
 	physical = true,
@@ -24,16 +80,16 @@ minetest.register_entity("knockout:entity", {
 	textures = {"invisible.png", "invisible.png", "invisible.png", "invisible.png", "invisible.png", "invisible.png"},
 	is_visible = true,
 	makes_footstep_sound = false,
-    automatic_rotate = false,
-    on_activate = function(e, sdata, dtime)
+   automatic_rotate = false,
+   on_activate = function(e, sdata, dtime)
 		e.grabbed_name = sdata
 		e.object:set_armor_groups({immortal = 1})
 		local p = minetest.get_player_by_name(e.grabbed_name)
 		if p ~= nil then
 			e.object:set_yaw(p:get_look_horizontal())
 		end
-    end,
-    on_punch = function(e, puncher, time_from_last_punch, tool_capabilities, dir)
+   end,
+   on_punch = function(e, puncher, time_from_last_punch, tool_capabilities, dir)
 		-- The player can't punch themselves
 		if puncher:get_player_name() == e.grabbed_name then return end
 		-- If punched with a water bucket revive
@@ -51,25 +107,22 @@ minetest.register_entity("knockout:entity", {
 			p:punch(puncher, time_from_last_punch, tool_capabilities, dir)
 			p:set_attach(e.object, "", {x = 0, y = 10, z = 0}, {x = 0, y = 0, z = 0})
 		end
-    end,
-    on_rightclick = function(e, clicker)
+   end,
+   on_rightclick = function(e, clicker)
 		local cName = clicker:get_player_name()
 		if cName == e.grabbed_name then return end
-		for carryer, carried in pairs(knockout.carrying) do
-			if carryer == cName or carried == e.grabbed_name then return end
-		end
-		victim = minetest.get_player_by_name(e.grabbed_name)
-		if victim then
-			victim:set_attach(clicker, "", {x = 0, y = 0, z = -15}, {x = 0, y = 0, z = 0})
-			knockout.carrying[cName] = e.grabbed_name
-			e.object:remove()
-		end
-    end,
-    on_step = function(e, dtime)
+      if clicker:get_player_control().sneak then
+         knockout.pick_up(cName, e.grabbed_name, clicker)
+      else
+         local fs = get_knocked_out_fs(e.grabbed_name)
+         minetest.show_formspec(cName, "knockout:knocked_out_player", fs)
+      end
+   end,
+   on_step = function(e, dtime)
 		if knockout.knocked_out[e.grabbed_name] == nil or minetest.get_player_by_name(e.grabbed_name) == nil then
 			e.object:remove()
 		end
-    end
+   end
 })
 
 -- Load knocked out players
@@ -83,6 +136,21 @@ end
 -- Save knocked out players
 knockout.save = function()
 	knockout.storage:set_string("knocked_out", minetest.serialize(knockout.knocked_out))
+end
+
+knockout.pick_up = function(carrierName, carriedName, carrierObj)
+   for carryer, carried in pairs(knockout.carrying) do
+      if carryer == carrierName or carried == carriedName then return end
+   end
+   local victim = minetest.get_player_by_name(carriedName)
+   if victim then
+      local parent = victim:get_attach()
+      if parent then
+         parent:remove()
+      end
+      victim:set_attach(carrierObj, "", {x = 0, y = 0, z = -15}, {x = 0, y = 0, z = 0})
+      knockout.carrying[carrierName] = carriedName
+   end
 end
 
 -- Drop a player
@@ -127,7 +195,7 @@ knockout.knockout = function(pName, duration)
 			position = {x = 0.5, y = 0.5},
 			scale = {x= -110, y= -110},
 			alignment = {x = 0, y = 0},
-		
+
 		})
 	end
 	-- No interacting for you, player
