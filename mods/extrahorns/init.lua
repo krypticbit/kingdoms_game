e8317903faa39b5496620c3155cf9acb0ed11aca
DonutNinja <35146412+DonutNinja@users.noreply.github.com>
2019-01-05 13:09:54 +0000

    possibly fixed indents

--- mods/extrahorns/init.lua
+++ mods/extrahorns/init.lua
@@ -1,41 +1,41 @@
 -- large horn
 minetest.register_craftitem ("extrahorns:largehorn", {
-    description = "Large Horn",
-    inventory_image = "extrahorns_largehorn.png",
-    groups = {instrument=1},	
-    on_use = function (itemstack, user)
-	minetest.sound_play ("extrahorns_largehorn", {
+	description = "Large Horn",
+	inventory_image = "extrahorns_largehorn.png",
+	groups = {instrument=1},	
+	on_use = function (itemstack, user)
+		minetest.sound_play ("extrahorns_largehorn", {
 		pos = user:getpos(),
-            	max_hear_distance = 450,
-            	gain = 1,
-        })
-    end,
+		max_hear_distance = 450,
+		gain = 1,
+		})
+	end,
 })
 
 minetest.register_craft ({
-    type   = "shapeless",
-    output = "extrahorns:largehorn",
-    recipe = {"soundblocks:smallhorn", "default:steel_ingot"}
+	type   = "shapeless",
+	output = "extrahorns:largehorn",
+	recipe = {"soundblocks:smallhorn", "default:steel_ingot"}
 })
 
 -- Warhorn
 
 minetest.register_craftitem ("extrahorns:warhorn", {
-    description = "War Horn" ,
-    inventory_image = "extrahorns_warhorn.png",
-    groups = {instrument=1},	
-    on_use = function (itemstack, user)
-        minetest.sound_play ("extrahorns_warhorn", {
-            pos= user:getpos() ,
-            max_hear_distance = 550,
-            gain = 1.2,
-        })
-    end,
+	description = "War Horn" ,
+	inventory_image = "extrahorns_warhorn.png",
+	groups = {instrument=1},	
+	on_use = function (itemstack, user)
+		minetest.sound_play ("extrahorns_warhorn", {
+		pos= user:getpos() ,
+		max_hear_distance = 550,
+		gain = 1.2,
+		})
+	end,
 })
 
 minetest.register_craft ({
-    output = "extrahorns:warhorn",
-    recipe = {
+	output = "extrahorns:warhorn",
+	recipe = {
 		{"extrahorns:largehorn", "", ""},
 		{"", "extrahorns:largehorn", ""},
 		{"", "", "extrahorns:largehorn"},
@@ -44,23 +44,23 @@ minetest.register_craft ({
 
 -- bagpipes
 minetest.register_craftitem ("extrahorns:bagpipes", {
-    description = "Bagpipes",
-    inventory_image = "extrahorns_bagpipe.png" ,
-    groups = {instrument=1},	
-    on_use = function (itemstack, user)
-        minetest.sound_play ("extrahorns_bagpipe", {
-            pos = user:getpos(),
-            max_hear_distance = 200,
-            gain = 1,
-        })
-    end,
+	description = "Bagpipes",
+	inventory_image = "extrahorns_bagpipe.png" ,
+	groups = {instrument=1},	
+	on_use = function (itemstack, user)
+		minetest.sound_play ("extrahorns_bagpipe", {
+		pos = user:getpos(),
+		max_hear_distance = 200,
+		gain = 1,
+		})
+	end,
 })
 
 minetest.register_craft ({
-    output = "extrahorns:bagpipes",
-    recipe = {
+	output = "extrahorns:bagpipes",
+	recipe = {
 		{ "default:stick","default:stick", "default:gold_ingot"} ,
 		{ "group:wool","group:wool", ""} ,
 		{ "group:wool", "group:wool", "default:stick" } ,
-    }
+	}
 })
