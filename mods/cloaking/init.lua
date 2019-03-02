0f1cd8445471ed26020830381aa0c4c7b09c23f3
luk3yx <luk3yx@users.noreply.github.com>
2019-03-02 19:50:28 +1300

    Update cloaking

--- mods/cloaking/init.lua
+++ mods/cloaking/init.lua
@@ -1,7 +1,7 @@
 --
 -- Minetest player cloaking mod
 --
--- © 2018 by luk3yx
+-- © 2019 by luk3yx
 --
 local path = minetest.get_modpath('cloaking')
 dofile(path .. '/core.lua')
@@ -13,4 +13,38 @@ end
 
 if minetest.get_modpath('irc') then
     dofile(path .. '/irc.lua')
+end
+
+-- Attempt to support older versions of Minetest
+local cloaked_chat = 'cloaking.enable_cloaked_chat'
+if minetest.settings and minetest.settings.get_bool then
+    cloaked_chat = minetest.settings:get_bool(cloaked_chat)
+else
+    cloaked_chat = minetest.setting_getbool(cloaked_chat)
+end
+
+-- Load cloaked chat if enabled
+if cloaked_chat or cloaked_chat == nil then
+    dofile(path .. '/cloaked-chat.lua')
+end
+
+-- Enforce security of logs
+table.insert(minetest.registered_on_chat_messages, 1, function(name, msg)
+    if msg:find('[\r\n]') then
+        minetest.chat_send_player(name,
+            'You cannot use newlines in chat messages.')
+        return true
+    end
+end)
+
+local log = minetest.log
+function minetest.log(level, text)
+    level = level:gsub('[\r\n]', '  ')
+    if text then
+        text  = text:gsub('[\r\n]', '  ')
+    else
+        text  = level
+        level = 'none'
+    end
+    return log(level, text)
 end
