d61c6364de30085bf357977da67173a221031f5c
piecubed <40523150+piecubed@users.noreply.github.com>
2019-05-11 19:29:16 -0400

    Add cat_o_nine_tails

--- mods/whip/init.lua
+++ mods/whip/init.lua
@@ -18,3 +18,23 @@ minetest.register_craft({
 })
 
 knockout.register_tool("whip:whip", 1, 5, 50)
+
+minetest.register_tool("whip:cat_o_nine_tails", {
+   description = "Cat o' nine tails",
+   inventory_image = "whip_whip.png",
+   range = 7.5,
+   tool_capabilities = {
+      full_punch_interval = 1.8,
+      damage_groups = {fleshy = 5}
+   }
+})
+
+minetest.register_craft({
+   output = "whip:cat_o_nine_tails",
+   recipe = {
+      {"default:glass", "",              ""},
+      {"default:glass", "default:glass", ""},
+      {"whip:whip",     "default:glass", "default:glass"}
+   }
+})
+knockout.register_tool("whip:whip", 1, 6, 60)
