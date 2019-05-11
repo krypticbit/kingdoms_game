9e61fd7dafe275cbf248351f154c73f9d78c63b8
Billy S <bbrus@comcast.net>
2019-05-11 09:58:09 -0400

    Added the Much Requested™ mod

---
+++ mods/whip/init.lua
@@ -0,0 +1,20 @@
+minetest.register_tool("whip:whip", {
+   description = "Whip",
+   inventory_image = "whip_whip.png",
+   range = 10.0,
+   tool_capabilities = {
+      full_punch_interval = 2.0,
+      damage_groups = {fleshy = 3}
+   }
+})
+
+minetest.register_craft({
+   output = "whip:whip",
+   recipe = {
+      {"", "xdecor:rope", ""},
+      {"xdecor:rope", "", "xdecor:rope"},
+      {"group:stick", "", ""}
+   }
+})
+
+knockout.register_tool("whip:whip", 1, 5, 50)
